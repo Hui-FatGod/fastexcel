1d83c5bf1ae64dd7e2534db03713450349cfe5ee
bhyue <bhyue@dces.cn>
2025-07-25 17:32:48 +0800

    feat: convert excel to pdf

--- fastexcel-core/src/main/java/cn/idev/excel/FastExcel.java
+++ fastexcel-core/src/main/java/cn/idev/excel/FastExcel.java
@@ -1,5 +1,10 @@
 package cn.idev.excel;
 
+import cn.idev.excel.fileconvertor.ExcelConverter;
+import cn.idev.excel.fileconvertor.FileConverterContext;
+
+import java.io.File;
+
 /**
  * This is actually {@link FastExcelFactory}, and short names look better.
  *
@@ -7,4 +12,17 @@ package cn.idev.excel;
  */
 public class FastExcel extends FastExcelFactory {
 
+    /**
+     * Convert excel to pdf
+     *
+     * @param excelFile excel file
+     * @param pdfFile   pdf file
+     * @param fontPath  font path for pdf can be null
+     * @param sheets    sheet index to convert, if null convert all sheets
+     */
+    public static void convertToPdf(File excelFile, File pdfFile, String fontPath, int[] sheets) {
+        FileConverterContext context = new FileConverterContext(excelFile, pdfFile, fontPath, sheets);
+        ExcelConverter excelConverter = context.getExcelConverter();
+        excelConverter.convertToPdf();
+    }
 }
